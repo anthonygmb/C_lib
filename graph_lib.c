8498a47ba29669dfc2dbf338bfe05f0ef4be5d4f
Anthony <anthonygumb.dev@gmail.com>
2016-11-03 09:37:35 +0100

    Ajout de la description de la fonction BFT

--- graph_lib.c
+++ graph_lib.c
@@ -24,10 +24,16 @@
 #define INFINITE -1;
 #define NIL -1;
 
+/**
+ * Enum du type de graphe
+ */
 typedef enum {ORIENTED, NON_ORIENTED} graph_type;
 
 typedef struct graphL *graphL;
 
+/**
+ * Structure du graphe
+ */
 struct graphL {
 	int size; // les sommets du graphe sont numérotés de 0 à size-1
 	graph_type type; // ORIENTED ou NON_ORIENTED
@@ -35,9 +41,10 @@ struct graphL {
 };
 
 /**
- * crée un graphe avec size sommets et de type graph_type
+ * Fonction de création de graphe avec size sommets et de type graph_type
  * @param size : nombre de sommets
  * @param type : type de graphe (ORIENTED ou NON_ORIENTED)
+ * @return G : un graphe initialisé
  */
 graphL makegraph(int size, graph_type type) {
 	int i;
@@ -64,7 +71,10 @@ graphL makegraph(int size, graph_type type) {
 }
 
 /*
- * ajoute à G un arc de source à dest
+ * Fonction d'ajout d'arc de source à dest au graphe G
+ * @param G : le graphe ou on doit ajouter un arc
+ * @param source : le sommet de départ de l'arc
+ * @param dest : le sommet de destination de l'arc
  */
 void add_arc(graphL G, int source, int dest) {
 	insert_tete(&(G -> adjacent[source]), dest);
@@ -74,7 +84,8 @@ void add_arc(graphL G, int source, int dest) {
 }
 
 /*
- * détruit G (en de-allouant la mémoire avec la fonction free)
+ * Fonction de destruction de graphe G (en de-allouant la mémoire avec la fonction free)
+ * @param G : le graphe à détruire
  */
 void destroy_graph(graphL *G) {
 	for(int i = 0; i < (*G) -> size; i++) {
@@ -86,7 +97,8 @@ void destroy_graph(graphL *G) {
 }
 
 /*
- * pour chaque sommet s, la fonction imprime s et sa liste d’adjacence
+ * Fonction pour imprimer les sommets d'un graphe G
+ * @param G : le graphe à imprimer
  */
 void print_graph(graphL G) {
 	if (G) {
@@ -99,7 +111,12 @@ void print_graph(graphL G) {
 }
 
 /*
- * Focntion de parcour en largeur d'un graphe
+ * Fonction de parcour en largeur d'un graphe
+ * @param G : le graphe à parcourir
+ * @param source : le sommet de début du parcours du graphe
+ * @param couleur[] : tableau des couleurs des sommets
+ * @param distance : distance d'un sommet depuis le sommet source
+ * @param parent[] : tableau des sommets parents de chaque sommet
  */
 void BFT(graphL G, int source, int couleur[], int distance[], int parent[]) {
 
